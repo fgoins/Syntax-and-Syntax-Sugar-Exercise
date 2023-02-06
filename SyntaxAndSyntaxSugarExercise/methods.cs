2cf74af6b93c7f6351ac76242b880a588347b482
Faithtorian Goins <faithtoriangoins@gmail.com>
2023-02-06 14:11:59 -0600

    string line

---
+++ SyntaxAndSyntaxSugarExercise/methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxAndSyntaxSugarExercise
+{
+    internal class methods
+    {
+        public static void main(string[] args) 
+        {
+            int answer = 4;
+            string response;
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " is greater than or equal to nine";
+            }
+            var answer1 = 4;
+            var response2 = (answer < 9) ? $"{answer} is less then 9" : $"{answer} is greater than or equal to nine";
+        }
+    }
+}
